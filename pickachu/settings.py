b669fb6542fb892d7193b989a93192a0ef67d436
cyned <cynedenis@gmail.com>
2017-09-28 11:00:25 +0300

    add navbar menu

--- pickachu/settings.py
+++ pickachu/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'home',
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -57,7 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR), 'templates'],
         'APP_DIRS': True,
-        
+
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
